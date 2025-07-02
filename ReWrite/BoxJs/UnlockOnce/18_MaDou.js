726f302bdcb9495d30e2ddc0e4b8b77b50d4ac22
DzWang <huisg74@gmail.com>
2025-07-02 21:11:30 +0800

    Update and rename 麻豆.js to 18_MaDou.js

---
+++ ReWrite/BoxJs/UnlockOnce/18_MaDou.js
@@ -0,0 +1,14 @@
+# 麻豆网址：https://d1skbu98kuldnf.cloudfront.net/
+
+
+[Script]
+ # 会员
+ http-response https://(d1skbu98kuldnf|dnt92ffcqr0xu).cloudfront.net/api/app/user/info script-path=https://raw.githubusercontent.com/ku-74/OpenQuanX/refs/heads/main/Source/18_MaDou.js, requires-body=true, timeout=60, tag=会员
+ # 视频
+ http-response https://(d1skbu98kuldnf|dnt92ffcqr0xu).cloudfront.net/api/app/media/play script-path=https://raw.githubusercontent.com/ku-74/OpenQuanX/refs/heads/main/Source/18_MaDou.js, requires-body=true, timeout=60, tag=视频
+ # 广告
+ http-response https://(d1skbu98kuldnf|dnt92ffcqr0xu).cloudfront.net/(api/app/ping/config|api/app/media/v5/home) script-path=https://raw.githubusercontent.com/ku-74/OpenQuanX/refs/heads/main/Source/18_MaDou.js, requires-body=true, timeout=60, tag=广告
+
+
+ [MITM]
+ hostname = *.cloudfront.net
