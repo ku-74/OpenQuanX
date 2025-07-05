00491fba3bc0c6eaf4f2860d10b3da2da1f9f5a4
DzWang <huisg74@gmail.com>
2025-07-05 18:33:45 +0800

    Update 18_WuXian.js

--- ReWrite/UnlockOnce/18_WuXian.js
+++ ReWrite/UnlockOnce/18_WuXian.js
@@ -4,7 +4,7 @@
 
 [rewrite_local]
 
-^https?:\/\/((cdn\.a2pi3\.com|xiapo6\.com|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(\/api\/vodapi\.html|\/Vodapi\.html|\/vodapi\.html))$ url script-request-body
+^https?:\/\/((cdn\.a2pi3\.com|xiapo6\.com|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(\/api\/vodapi\.html|\/Vodapi\.html|\/vodapi\.html))$ url script-request-body https://raw.githubusercontent.com/ku-74/OpenQuanX/refs/heads/main/ReWrite/UnlockOnce/18_WuXian.js
 
 [mitm]
 hostname = cdn.a2pi3.com, xiapo6.com
